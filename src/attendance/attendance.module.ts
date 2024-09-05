15f1406adb92a1df0363f1eadad876dc0463ecf6
Lê Hồng Minh <minhle20044@gmail.com>
2024-09-05 16:50:16 +0700

    Import module Schedule

--- src/attendance/attendance.module.ts
+++ src/attendance/attendance.module.ts
@@ -7,10 +7,11 @@ import { Attendance } from '../entity/attendance.entity';
 import { StudentList } from 'src/entity/studentlist.entity';
 import { Class } from 'src/entity/class.entity';
 import { User } from 'src/entity/user.entity';
+import { Schedule } from 'src/entity/schedule.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Attendance, StudentList, Class, User]),
+    TypeOrmModule.forFeature([Attendance, StudentList, Class, User, Schedule]),
   ],
   providers: [AttendanceService],
   controllers: [AttendanceController],

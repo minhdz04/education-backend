2b670c4bc2c5476af915c7cba429825f50d3810e
Lê Hồng Minh <minhle20044@gmail.com>
2024-11-27 23:05:26 +0700

    Initial commit

--- src/tun/tun.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('tun')
-export class TunController {}
--- src/tun/tun.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TunController } from './tun.controller';
-
-@Module({
-  controllers: [TunController]
-})
-export class TunModule {}
